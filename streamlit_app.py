81349045196324275b846bfe2aa44e01dd6ac7c0
Diego Saenz <41396886+dssaenzml@users.noreply.github.com>
2023-07-03 16:30:44 +0400

    display submenu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,16 @@
 import pandas as pd
-import streamlit as sl
+import streamlit as st
 
-sl.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
 
-sl.header('Breakfast Favorites')
-sl.text('🥣 Omega 3 & Blueberry Oatmeal')
-sl.text('🥗 Kale, Spinach & Rocket Smoothie')
-sl.text('🐔 Hard-Boiled Free-Range Egg')
-sl.text('🥑🍞 Avocado Toast')
+st.header('Breakfast Favorites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
 
-sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# create submenu
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
