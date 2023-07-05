aa2d81ee8d750e028fb117656ed716783c3c594b
Diego Saenz <41396886+dssaenzml@users.noreply.github.com>
2023-07-05 14:59:34 +0400

    added user implementation

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,10 @@ st.dataframe(fruits_to_show)
 
 # Fruityvice data
 st.header("Fruityvice Fruit Advice!")
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice.lower())
 
 # normalize the json object
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
