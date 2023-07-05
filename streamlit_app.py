d3a827c462c80cbfa3ec7f1be5235ae922068910
Diego Saenz <41396886+dssaenzml@users.noreply.github.com>
2023-07-05 14:56:44 +0400

    added dataframe format to api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,9 @@ st.dataframe(fruits_to_show)
 st.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+st.text(fruityvice_response.json()) # just writes the data to the screen
+
+# normalize the json object
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# shows json response as dataframe
+st.dataframe(fruityvice_normalized)
